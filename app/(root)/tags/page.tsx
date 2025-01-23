985f577a4d8824447e0a1d3fa7798b2ea8874065
Koushik2208 <gorrekoushik@gmail.com>
2025-01-23 11:16:02 +0530

    Implement get tags server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,6 +1,18 @@
+import { getTags } from "@/lib/actions/tag.action";
 import React from "react";
+import { json } from "stream/consumers";
 
-const Tags = () => {
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "",
+  });
+
+  const { tags } = data || {};
+
+  console.log("TAGS", JSON.stringify(tags, null, 2));
+
   return <div>Tags</div>;
 };
 
